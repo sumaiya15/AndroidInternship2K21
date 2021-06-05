d85db6d01cc70a23429ebb915cbfeacc9ad4560a
Sumaiya ansari <sumaiyaansaricse.gist@gmail.com>
2021-06-05 18:22:11 +0530

    added

---
+++ AppBarWithOptionsMenu/app/src/main/java/com/example/appbarwithoptionsmenu/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.appbarwithoptionsmenu;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.mymenu,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.profile:
+                Toast.makeText(this, item.getTitle(), Toast.LENGTH_SHORT).show();
+            case R.id.contact:
+                Toast.makeText(this, item.getTitle(), Toast.LENGTH_SHORT).show();
+            case R.id.settings:
+                Toast.makeText(this, item.getTitle(), Toast.LENGTH_SHORT).show();
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
